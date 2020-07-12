afcda6b39061a71c08eae275e698a720cfaef5bc
Aidan Traboulay <aidantraboulay13@gmail.com>
2020-07-12 03:38:14 -0400

    added code to create dir for song files

--- converter.py
+++ converter.py
@@ -5,25 +5,36 @@ import subprocess
 
 app = Flask(__name__)
 
+# Index app route
 @app.route('/', methods=['POST', 'GET'])
 def createPlaylist():
+
+    # Initialize local variables
     playlistName = ''
     songURL = ' '
     songName = ' '
+    error_message = ''
 
+    # If form request sent (button pressed)
     if request.method == 'POST':
 
+        # Get form information
         playlistName = request.form.get('playlistName')
-
         songURL = request.form.get('songURL')
-
         songName = request.form.get('songName')
 
+        # Check if playlist name is not empty
+        if playlistName != "":
+            try:
+                os.mkdir(os.path.expanduser('~') + '\\Desktop\\' + playlistName)
+            except(FileExistsError):
+                error_message = 'This playlist folder already exists!'
+
+        # Use youtube-dl to download song at specific file path 
         subprocess.call(['youtube-dl', '-o', os.path.expanduser('~') + '\\Desktop\\' + songName + ".mp3",
         "--extract-audio", "--audio-format", "mp3", songURL])
 
-        # os.mkdir(r'C:\Users\Aidan\Desktop\folder')
-
+    # Return html page
     return render_template('index.html', playlistName=playlistName, songName=songName)
 
 if __name__ == "__main__":
